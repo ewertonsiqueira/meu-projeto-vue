9438798ce16eb1acc346ed4f858d16b30c6eb8b9
Ewerton Siqueira <ewertoncostaxx@hotmail.com>
2025-04-07 22:32:46 -0300

    init

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,41 @@
+import { fn } from '@storybook/test';
+
+import MyButton from './Button.vue';
+
+export default {
+  title: 'Example/Button',
+  component: MyButton,
+  tags: ['autodocs'],
+  argTypes: {
+    size: { control: { type: 'select' }, options: ['small', 'medium', 'large'] },
+    backgroundColor: { control: 'color' },
+  },
+  args: { onClick: fn() },
+};
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Buttonss',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
