9e0ba873bf2fda5bc691b67770150a33cdc99f83
Ewerton Siqueira <ewerton@rotaexata.com.br>
2025-04-11 14:40:48 -0300

    atualizando para usar setup

--- src/components/RButton.vue
+++ src/components/RButton.vue
@@ -9,76 +9,61 @@
   </button>
 </template>
 
-<script>
-import { reactive, computed, watch } from 'vue';
+<script setup>
+import { computed, watch, defineProps } from 'vue';
 
-
-export default {
-  name: 'RButton',
-
-  props: {
-    label: {
-      type: String,
-      required: true,
-    },
-    primary: {
-      type: Boolean,
-      default: false,
-    },
-    size: {
-      type: String,
-      validator: function (value) {
-        return ['small', 'medium', 'large'].indexOf(value) !== -1;
-      },
-    },
-    backgroundColor: {
-      type: String,
-    },
-    disabled: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-    loading: {
-      type: Boolean,
-      required: false,
-      default: false
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  primary: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: String,
+    validator: function (value) {
+      return ['small', 'medium', 'large'].indexOf(value) !== -1;
     },
   },
-
-  emits: ['click'],
-
-  setup(props, { emit }) {
-    props = reactive(props);
-
-    watch(props.disabled, (newValue) => {
-      if (newValue) {
-        alert('Button is disabled');
-        console.log('Button is disabled');
-      } else {
-        console.log('Button is enabled');
-      }
-    });
-    
-    return {
-      classes: computed(() => ({
-        'storybook-button': true,
-        'storybook-button--disabled': props.disabled,
-        'storybook-button--primary': props.primary,
-        'storybook-button--secondary': !props.primary,
-        [`storybook-button--${props.size || 'medium'}`]: true,
-      })),
-      style: computed(() => ({
-        backgroundColor: props.backgroundColor,
-        minWidth: 100
-      })),
-      onClick() {
-        if (props.disabled) return
-        emit('click');
-      },
-    };
+  backgroundColor: {
+    type: String,
   },
-};
+  disabled: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  loading: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+})
+
+const emit = defineEmits(['click']);
+
+const classes = computed(() => {
+  return {
+    'storybook-button': true,
+    'storybook-button--disabled': props.disabled,
+    'storybook-button--primary': props.primary,
+    'storybook-button--secondary': !props.primary,
+    [`storybook-button--${props.size || 'medium'}`]: true,
+  }
+})
+
+const style = computed(() => { return { 
+  backgroundColor: props.backgroundColor,
+  minWidth: 100
+}})
+
+function onClick() {
+  if (props.disabled) return
+  emit('click');
+}
+
 </script>
 
 <style>
